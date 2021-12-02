1e6e8a5581ecab0ac03abeebf526a920683b942f
Otieno <36030016+otienosteve@users.noreply.github.com>
2021-12-02 18:36:23 +0300

    hit foodish api from my app and displayed images on my website

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ export default function App() {
   const [data, setData] = useState({ message: "welcome" });
 
   const fetchData = async () => {
-    try{
-      const resp = await fetch("https://axoltlapi.herokuapp.com/");
+    try {
+      const resp = await fetch("https://foodish-api.herokuapp.com/api/");
       const quotes = await resp.json();
-      console.log(quotes.results);
-      setData(quotes.results);
-
-    }catch(error){
-      setData({message:"Cannot communicate with The API "+error})
+      console.log(quotes);
+      setData(quotes);
+    } catch (error) {
+      setData({ message: "Cannot communicate with The API " + error });
     }
   };
   useEffect(() => {
     fetchData();
   }, []);
-  return (<><h1>{data.message}</h1>
+  return (
+    <>
+      <img src={data.image} alt="food" />
     </>
   );
 }
