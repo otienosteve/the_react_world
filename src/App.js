2f61315f75a67d001c7f97384a3ca74a1eeb6026
Otieno <36030016+otienosteve@users.noreply.github.com>
2021-12-02 17:19:32 +0300

    fetched data from axolt API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import "./styles.css";
 import { useState, useEffect } from "react";
-import Quotes from "./Components/Quotes";
+
 export default function App() {
   const [data, setData] = useState({ message: "welcome" });
 
   const fetchData = async () => {
-    const resp = await fetch("https://randomuser.me/api/");
-    const quotes = await resp.json();
-    //console.log(quotes.results);
-    setData(quotes.results);
+    try{
+      const resp = await fetch("https://axoltlapi.herokuapp.com/");
+      const quotes = await resp.json();
+      console.log(quotes.results);
+      setData(quotes.results);
+
+    }catch(error){
+      setData({message:"Cannot communicate with The API "+error})
+    }
   };
   useEffect(() => {
     fetchData();
   }, []);
-  return (
-    <>
-      <Quotes data={data} fetchData={fetchData} />
+  return (<><h1>{data.message}</h1>
     </>
   );
 }
